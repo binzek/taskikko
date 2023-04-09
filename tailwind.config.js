436d519d59b267fa1fb7b55f6e6bc658f7db3518
Abdul Wajid <abdulwajidnv@gmail.com>
2023-04-09 14:58:37 +0530

    Cleaned up the file structure. Title, description and favicon added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "palette-grey": "#F4F4F4",
+        "palette-mint": "#6DECB9",
+        "palette-teal": "#11999E",
+        "palette-black": "#3C3C3C",
+      },
+    },
+  },
+  plugins: [],
+};

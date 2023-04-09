7e59ed214efa3b145bd67e96627d2a6eaf5d1d55
Abdul Wajid <abdulwajidnv@gmail.com>
2023-04-09 15:16:30 +0530

    Base App component styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <body className={""}>
+      <div className="min-h-screen bg-palette-grey dark:bg-palette-black">
+        App
+      </div>
+    </body>
+  );
 };
 
 export default App;

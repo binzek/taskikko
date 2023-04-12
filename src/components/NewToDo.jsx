f540328d0d714223da65147f463f94466bc0add9
Abdul Wajid <abdulwajidnv@gmail.com>
2023-04-12 14:47:44 +0530

    Added empty input validation

--- src/components/NewToDo.jsx
+++ src/components/NewToDo.jsx
@@ -3,24 +3,33 @@ import { AiOutlinePlus } from "react-icons/ai";
 
 const NewToDo = ({ setToDos, toDos }) => {
   const [newToDo, setNewToDo] = useState("");
+  let inputValue = false;
 
   return (
-    <div className="relative mt-2 flex items-center justify-between px-5 py-2">
+    <form
+      className="relative mt-2 flex items-center justify-between px-5 py-2"
+      onSubmit={(e) => {
+        e.preventDefault();
+        setToDos([...toDos, newToDo]);
+        setNewToDo("");
+      }}
+    >
       <input
         value={newToDo}
         onChange={(e) => setNewToDo(e.target.value)}
         type="text"
-        className="w-full border-b-gunmetal border-opacity-40 bg-transparent pb-1 pl-1 pr-6 font-light text-gunmetal outline-none focus:border-b dark:border-b-azure dark:border-opacity-30 dark:text-azure"
+        id="newToDo"
+        className="w-full border-b-gunmetal border-opacity-40 bg-transparent pb-1 pl-1 pr-6 font-light text-gunmetal outline-none focus:border-b invalid:focus:border-b-raspberry invalid:focus:placeholder:text-raspberry dark:border-b-azure dark:border-opacity-30 dark:text-azure dark:invalid:focus:border-b-raspberry"
         placeholder="Type new task"
+        required
       />
-      <AiOutlinePlus
+      <button
+        type="submit"
         className="absolute right-5 cursor-pointer text-lg text-gunmetal opacity-60 dark:text-azure"
-        onClick={() => {
-          setNewToDo("");
-          setToDos([...toDos, newToDo]);
-        }}
-      />
-    </div>
+      >
+        <AiOutlinePlus />
+      </button>
+    </form>
   );
 };
 
